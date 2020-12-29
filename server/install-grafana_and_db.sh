30576d3a57541964b4571efecb7812066daa6ef6
Efesto <polita.marco@gmail.com>
2020-12-29 16:28:56 +0100

    adds server config from life_quality_data

---
+++ server/install-grafana_and_db.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# packages
+sudo apt-get update
+sudo apt-get install git wget -y
+
+# pull repository
+git clone https://github.com/Efesto/home_telemetry.git ~/home_telemetry
+cd ~/life_quality_data/server
+
+# install influx db
+make install-db
+
+# install Grafana
+make install-grafana
+
+# configure nginx
+make install-nginx
+
