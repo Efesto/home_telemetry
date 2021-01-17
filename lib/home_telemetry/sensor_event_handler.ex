48d1bee00089cf650e223674beb4ea9906fe5ca9
Efesto <polita.marco@gmail.com>
2021-01-17 13:11:52 +0100

    adds telemetry event handler and series for ccs811

--- lib/home_telemetry/sensor_event_handler.ex
+++ lib/home_telemetry/sensor_event_handler.ex
@@ -1,5 +1,6 @@
 defmodule HomeTelemetry.SensorEventHandler do
   alias HomeTelemetry.Series.DHT22
+  alias HomeTelemetry.Series.CCS811
 
   require Logger
 
@@ -15,10 +16,22 @@ defmodule HomeTelemetry.SensorEventHandler do
     })
   end
 
+  def handle_event([:ccs811, :read], %{co2: co2, tvoc: tvoc}, _metadata, _config) do
+    Logger.info("CO2: #{co2} ppm")
+    Logger.info("TVOC: #{tvoc} ppm")
+
+    data = %CCS811{}
+
+    HomeTelemetry.SeriesConnection.write(%{
+      data
+      | fields: %{data.fields | co2: co2, tvoc: tvoc}
+    })
+  end
+
   def attach do
-    :telemetry.attach(
-      "dht_reader",
-      [:dht, :read],
+    :telemetry.attach_many(
+      "sensors_read_handler",
+      [[:dht, :read], [:ccs811, :read]],
       &HomeTelemetry.SensorEventHandler.handle_event/4,
       nil
     )
