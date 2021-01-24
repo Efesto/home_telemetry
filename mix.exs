4cdf185a1d2e5649f324f49c8aaba0071025eeec
Efesto <polita.marco@gmail.com>
2021-01-24 18:47:14 +0100

    uses ccs811 library

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule HomeTelemetry.MixProject do
       {:instream, "~> 1.0"},
       {:ssh_subsystem_fwup, "~> 0.5.0", runtime: false},
       {:circuits_i2c, "~> 0.3.7"},
-      {:ccs811, path: "../ccs811"},
+                               {:ccs811, "~> 0.1.0"},
 
       # Dependencies for all targets except :host
       {:nerves_runtime, "~> 0.11.3", targets: @all_targets},
